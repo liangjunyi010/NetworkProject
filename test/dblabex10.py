c970d9834aed1caa08ca96330daa7f661dfe1cf5
Liu Lingao <56510787+lingaoliu105@users.noreply.github.com>
2023-04-05 00:08:11 +0800

    delete redundant file

--- test/dblabex10.py
+++
@@ -1,8 +0,0 @@
-from functools import reduce
-def min(li):
-    return reduce(lambda x,y:x if x<=y else y,li)
-def max(li):
-    return reduce(lambda x,y:x if x>=y else y,li)
-print(min([32,63,7,10,100]))
-print(max([32,63,7,10,100]))
-
