8ec1acb0af214db2e33ea0a7116f8d70df887ab0
M-Rues <yuqin.110601@gmail.com>
2023-04-05 15:04:07 +0800

    server

--- src/main/ftp/server.js
+++ src/main/ftp/server.js
@@ -1,5 +1,6 @@
 const FtpSrv = require("ftp-srv");
 const config = require("../../common/config.json");
+const net = require('net');
 // import * as config from "../../common/config.json";
 
 class FtpFileTransferServer {
@@ -32,8 +33,41 @@ class FtpFileTransferServer {
     this.ftpServer.listen().then(() => {
       console.log("Ftp server is starting...");
     });
-  }
 
+    // tcp server to receive filname and cut
+  const server = net.createServer()//客户端有人连接的时候触发
+  server.on('connection',(socket)=>{//当前链接的socket对象
+      console.log('TCP client connected');
+
+      //监听客户端数据
+      socket.on('data',data=>{
+          console.log(data.toString());
+          fs.readFile(data.toString(),(err,file_data)=>{
+              // 判断是否读取成功
+              if(err){
+                  return console.log('Read File Failed on TCP server！'+err.message)
+              }
+              console.log('Read File Successfully on TCP server' ) // the content of file is stored in variable: dataStr
+              let Dir = path.join(__dirname,`../log`);//创建目录
+              fs.mkdirSync(Dir,{
+                  //是否使用递归创建目录
+                  recursive:true
+              })
+              let directory = Dir + '/dataStr.log'
+              console.log(file_data)
+              fs.writeFile(directory, file_data, function (err,data) {
+                  console.log('write in file successfully')
+                  console.log(file_data)
+              });
+
+          })
+      })
+
+  })
+
+  server.listen(9000,'127.0.0.1')
+  }
+  
   shutDown() {
     this.ftpServer.close();
   }
